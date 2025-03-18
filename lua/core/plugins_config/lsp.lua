71ed426390211dbb3921d14e38ec38324f3f4123
emmanueltudela <emmanueltudela.pro@gmail.com>
2025-03-18 23:36:01 +0100

    Added keymap for floating error message

--- lua/core/plugins_config/lsp.lua
+++ lua/core/plugins_config/lsp.lua
@@ -4,6 +4,7 @@ require("mason-lspconfig").setup({})
 local on_attach = function(_, bufnr)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+  vim.keymap.set('n', 'ge', vim.diagnostic.open_float, {})
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
