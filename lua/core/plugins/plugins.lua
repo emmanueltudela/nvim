f6b0c3653e76813ae507eea57364fb7b721b15a0
emmanueltudela <emmanueltudela.pro@gmail.com>
2025-09-05 19:55:11 +0200

    Listed some plugins together under lua/core/plugins/plugins.lua

---
+++ lua/core/plugins/plugins.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-neorg/neorg",
+    "nvim-telescope/telescope.nvim",
+
+    -- Nerd tree
+    "nvim-tree/nvim-tree.lua",
+    "nvim-tree/nvim-web-devicons",
+
+    -- Treesitter
+    "nvim-treesitter/nvim-treesitter",
+    "pest-parser/pest.vim",
+}
--- lua/core/plugins/treesitter.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  "pest-parser/pest.vim"
-}
